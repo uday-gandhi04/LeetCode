0e242f351fa265ecf3df11c3bec283a026c365b8
uday-gandhi04 <121980334+uday-gandhi04@users.noreply.github.com>
2025-05-31 04:26:49 +0530

    Time: 734 ms (54.95%) | Memory: 18.3 MB (63.77%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         out=[]
         nums=sorted(nums)
-        print(nums)
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
