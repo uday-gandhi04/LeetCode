0e1a256d25f3d0a8f8d89da8e25c77b3a6eb202e
uday-gandhi04 <121980334+uday-gandhi04@users.noreply.github.com>
2025-05-31 15:20:39 +0530

    Time: 119 ms (10.32%) | Memory: 12.4 MB (68.19%) - LeetSync

---
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def snakesAndLadders(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: int
+        """
+
+        queue=deque()
+        n=len(board)
+        queue.append(0)
+        visited=set()
+        level=0
+        arr=[]
+        for i in range(n-1,-1,-1):
+            for j in range(n):
+                if (n-1-i)%2==0:
+                    arr.append(board[i][j])
+                else:
+                    arr.append(board[i][-j-1])
+
+        print(arr)
+
+        while queue:
+            sz=len(queue)
+            while sz:
+                curr=queue.popleft()
+                if curr in visited:
+                    sz-=1
+                    continue
+                visited.add(curr)
+                for i in range(1,7):
+                    if curr+1==n**2:
+                        return level
+                    if curr+i<n**2:
+                        if arr[curr+i]==-1:
+                            queue.append(curr+i)
+                        else:
+                            queue.append(arr[curr+i]-1)
+                sz-=1
+            level+=1
+        return -1
