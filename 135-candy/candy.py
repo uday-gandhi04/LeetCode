1ee236613f66c741d2a8f689a82f3e77521fd0b3
uday-gandhi04 <121980334+uday-gandhi04@users.noreply.github.com>
2025-06-02 12:41:07 +0530

    Time: 229 ms (6.62%) | Memory: 14.7 MB (73.87%) - LeetSync

---
+++ 135-candy/candy.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        n=len(ratings)
+        arr=[1]*n
+
+        for i in range(1,n):
+            if ratings[i-1]<ratings[i] and arr[i-1]>=arr[i]:
+                arr[i]=arr[i-1]+1
+        for i in range(n-2,-1,-1):
+            if ratings[i+1]<ratings[i] and arr[i+1]>=arr[i]:
+                arr[i]=arr[i+1]+1
+        
+        print(arr)
+        
+        return sum(arr)
+        
