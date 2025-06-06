c3361b8415bffce952b8210a4b66aed3fa51aa54
uday-gandhi04 <121980334+uday-gandhi04@users.noreply.github.com>
2025-06-06 17:52:30 +0530

    Time: 1355 ms (58.82%) | Memory: 15.3 MB (94.12%) - LeetSync

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -6,23 +6,18 @@ class Solution(object):
         stack = []
         result = []
 
-        # Start tracking from 'a'
         min_ord = ord('a')
 
         for c in s:
             stack.append(c)
             freq[c] -= 1
 
-            # Move min_ord forward if that char is not present in freq anymore
             while min_ord <= ord('z') and freq[chr(min_ord)] == 0:
                 min_ord += 1
 
-            # Pop from stack if it's <= the current minimum char left
             while stack and ord(stack[-1]) <= min_ord:
                 result.append(stack.pop())
 
-        # Add remaining stack elements
-        while stack:
-            result.append(stack.pop())
+        result.extend(stack[::-1])
 
         return "".join(result)
