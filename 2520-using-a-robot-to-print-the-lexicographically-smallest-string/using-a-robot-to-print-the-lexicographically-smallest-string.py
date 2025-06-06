1ca0fc804ea3a66e567e7f5545ca967c9add4dc8
uday-gandhi04 <121980334+uday-gandhi04@users.noreply.github.com>
2025-06-06 17:45:35 +0530

    Time: 5979 ms (5.88%) | Memory: 14.6 MB (100.00%) - LeetSync

---
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def robotWithString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        from collections import Counter
+
+        freq=Counter(s)
+        stack=[]
+        p=""
+
+        def minchar():
+            for i in range(26):
+                idx=chr(ord('a')+i)
+                if freq[idx]!=0:
+                    return idx
+            return 'a'
+
+        for i in s:
+            stack.append(i)
+            freq[i]-=1
+            while stack and stack[-1]<=minchar():
+                p+=stack.pop()
+        
+        while stack:
+            p+=stack.pop()
+        
+        return p
