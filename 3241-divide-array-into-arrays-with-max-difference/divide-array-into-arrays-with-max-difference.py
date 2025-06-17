725748633fa01b6a90bba943a8b38f8ca1bc43c7
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-06-17 20:20:16 +0530

    Time: 122 ms (60.00%) | Memory: 31.7 MB (5.00%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.py
@@ -10,13 +10,12 @@ class Solution(object):
 
         temp=[]
 
-        for i in nums:
-            temp.append(i)
-            if len(temp)==3:
-                if temp[-1]-temp[0]>k:
-                    return []
-                out.append(temp)
-                temp=[]
+        for i in range(0,len(nums),3):
+            temp.extend([nums[i],nums[i+1],nums[i+2]])
+            if temp[-1]-temp[0]>k:
+                return []
+            out.append(temp)
+            temp=[]
         
         for i in out:
             if i[-1]-i[0]>k:
