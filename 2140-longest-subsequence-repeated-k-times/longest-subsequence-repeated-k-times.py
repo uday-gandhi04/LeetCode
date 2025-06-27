9a9912713dd595021ce1f537371645dc78a370bf
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-06-27 20:55:49 +0530

    Time: 1225 ms (100.00%) | Memory: 12.4 MB (100.00%) - LeetSync

---
+++ 2140-longest-subsequence-repeated-k-times/longest-subsequence-repeated-k-times.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestSubsequenceRepeatedK(self, s, k):
+        ans = ""
+        candidate = sorted(
+            [c for c, w in Counter(s).items() if w >= k], reverse=True
+        )
+        print(candidate)
+        q = deque(candidate)
+        while q:
+            curr = q.popleft()
+            if len(curr) > len(ans):
+                ans = curr
+            # generate the next candidate string
+            for ch in candidate:
+                nxt = curr + ch
+                it = iter(s)
+                if all(ch in it for ch in nxt * k):
+                    q.append(nxt)
+        return ans
