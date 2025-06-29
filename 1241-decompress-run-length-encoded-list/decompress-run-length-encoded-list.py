39555fc3bbd2aec3e2ec6fbc822b9a67744b7171
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-06-30 03:06:04 +0530

    Time: 3 ms (32.12%) | Memory: 12.8 MB (38.34%) - LeetSync

--- 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.py
+++ 1241-decompress-run-length-encoded-list/decompress-run-length-encoded-list.py
@@ -7,6 +7,9 @@ class Solution(object):
         out=[]
 
         for i in range(0,len(nums),2):
-            out.extend([nums[i+1]]*nums[i])
+            freq=nums[i]
+            val=nums[i+1]
+            for j in range(freq):
+                out.append(val)
         return out
         
