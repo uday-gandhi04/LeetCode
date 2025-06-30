e5a353e8294c20de8e2187338e070427070afbd9
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-07-01 01:21:08 +0530

    Time: 120 ms (78.57%) | Memory: 21.8 MB (78.57%) - LeetSync

--- 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
+++ 3262-find-polygon-with-the-largest-perimeter/find-polygon-with-the-largest-perimeter.py
@@ -6,15 +6,12 @@ class Solution(object):
         """
         nums.sort()
         n=len(nums)
-        prefix=[0]*(n+1)
-
-        for i in range(n):
-            prefix[i+1]=prefix[i]+nums[i]
-        out=nums[0]+nums[1]
+        summ=sum(nums)
 
         for i in range(n-1,-1,-1):
-            if prefix[i]>nums[i] and prefix[i+1]>out: 
-                return prefix[i+1]
+            summ-=nums[i]
+            if summ>nums[i]:
+                return summ+nums[i]
         return -1
 
             
