5d7b6277072f6881fef62bfae1a4c863bd11e86b
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-07-08 16:39:23 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (72.64%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -10,19 +10,14 @@ class Solution(object):
 
         firstrow=False
         firstcol=False
-
-        for i in range(n):
-            if matrix[0][i]==0:
-                firstrow=True
-                break
-        for i in range(m):
-            if matrix[i][0]==0:
-                firstcol=True
-                break
         
         for i in range(m):
-            for j in range(n):
+            for j in range(n):  
                 if matrix[i][j]==0:
+                    if i==0:
+                        firstrow=True
+                    if j==0:
+                        firstcol=True
                     matrix[0][j]=0
                     matrix[i][0]=0
         
