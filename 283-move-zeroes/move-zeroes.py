1662ca73a2531382375b61fefb73ebc315d33fa6
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-07-08 16:49:53 +0530

    Time: 1 ms (93.90%) | Memory: 13.7 MB (37.33%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -4,14 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        i=0
-        j=0
-        n=len(nums)
-        while i < n and j <n:
-            while i< n-1 and nums[i]!=0:
-                i+=1
-            j=i+1
-            while j<n and nums[j]==0:
-                j+=1
-            if j<n:
-                nums[i],nums[j]=nums[j],nums[i]
+        slow = 0  # next index for a non zero elem, guarantee no zeros come before slow
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[fast], nums[slow] = nums[slow], nums[fast]
+                slow += 1
