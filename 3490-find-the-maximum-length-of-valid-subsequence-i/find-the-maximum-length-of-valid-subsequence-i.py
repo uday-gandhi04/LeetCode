ab8ed850ede8531a3b08f1f30ebe06a4a7b72742
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-07-16 19:12:05 +0530

    Time: 60 ms (54.55%) | Memory: 24.3 MB (100.00%) - LeetSync

--- 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
+++ 3490-find-the-maximum-length-of-valid-subsequence-i/find-the-maximum-length-of-valid-subsequence-i.py
@@ -4,49 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        """def checkEven(nums):
-            out=0
-
-            for num in nums:
-                if num^1==num+1:
-                    out+=1
-            return out
-        
-        def checkOdd(nums):
-            out=0
-            for num in nums:
-                if num^1!=num+1:
-                    out+=1
-            return out
-
-        def checkEvenOdd(nums):
-            out=0
-            odd=False
-
-            for num in nums:
-                if num^1==num+1 and not odd:
-                    out+=1
-                    odd=True
-                elif num^1!=num+1 and odd:
-                    out+=1
-                    odd=False
-            return out
-
-
-        
-        def checkOddEven(nums):
-            out=0
-            odd=False
-
-            for num in nums:
-                if num^1==num+1 and odd:
-                    out+=1
-                    odd=False
-                elif num^1!=num+1 and not odd:
-                    out+=1
-                    odd=True
-            return out"""
-        
 
         checkEven=0
         checkOdd=0
