51033704df548a55a596c33866b9f78029c87896
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-07-23 03:27:15 +0530

    Time: 588 ms (51.72%) | Memory: 17.5 MB (22.74%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -7,7 +7,7 @@ class Solution(object):
         n=len(s)
         out=[]
         for i in range(n):
-            if i<2 or not (s[i]==s[i-1] and s[i-1]==s[i-2]):
+            if i<2 or (s[i]!=s[i-1] or s[i-1]!=s[i-2]):
                 out.append(s[i])
         return ''.join(out)
         
