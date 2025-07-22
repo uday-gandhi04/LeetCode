997c0c67b4244a16a46ce0ff123e18c62452b1f0
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-07-23 03:26:46 +0530

    Time: 587 ms (51.81%) | Memory: 17.4 MB (22.74%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -5,9 +5,9 @@ class Solution(object):
         :rtype: str
         """
         n=len(s)
-        out=s[:2]
-        for i in range(2,n):
-            if not (s[i]==s[i-1] and s[i-1]==s[i-2]):
-                out+=s[i]
-        return out
+        out=[]
+        for i in range(n):
+            if i<2 or not (s[i]==s[i-1] and s[i-1]==s[i-2]):
+                out.append(s[i])
+        return ''.join(out)
         
