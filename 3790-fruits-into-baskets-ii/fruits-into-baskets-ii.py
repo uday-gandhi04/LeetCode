b8eab1868c564254ee81a41edb623a682e5ab287
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-08-07 03:22:53 +0530

    Time: 38 ms (48.49%) | Memory: 12.5 MB (57.54%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.py
@@ -7,13 +7,11 @@ class Solution(object):
         """
         out=0
         for fruit in fruits:
-            flag=False
             for i in range(len(baskets)):
                 if baskets[i]>=fruit:
                     baskets[i]=0
-                    flag=True
                     break
-            if not flag:
+            else:
                 out+=1
         return out
 
