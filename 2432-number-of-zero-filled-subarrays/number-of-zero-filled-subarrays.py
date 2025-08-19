cca33a14353510c0475157f7b6255c2c7eb51e98
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-08-20 03:30:41 +0530

    Time: 39 ms (73.91%) | Memory: 18.4 MB (66.52%) - LeetSync

---
+++ 2432-number-of-zero-filled-subarrays/number-of-zero-filled-subarrays.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def zeroFilledSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        arr=[]
+        i=0
+        count=0
+        out=0
+        for num in nums:
+            if num==0:
+                count=count+i+1
+                i+=1
+            else:
+                out+=count
+                count=0
+                i=0
+        
+        return out+count
+        
