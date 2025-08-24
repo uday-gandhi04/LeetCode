0a7e1cb6fca46b26a082f82d6ba74d156acea59a
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-08-25 01:49:07 +0530

    Time: 70 ms (68.98%) | Memory: 14.4 MB (94.05%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.py
@@ -27,7 +27,6 @@ class Solution(object):
                     count-=1     
                 i+=1
                 noZero-=1
-            print(i,j,count)
             j+=1
         
         out=max(out,count)
