e01cf02bf03a4fc5f5e7ee8df0b4e40e050a0d81
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-09-02 22:44:48 +0530

    Time: 209 ms (66.67%) | Memory: 12.5 MB (33.33%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def numberOfPairs(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points.sort(key=lambda x:(x[0],-x[1]))
+        print(points)
+        out=0
+
+        for p in range(1,len(points)):
+            for i in range(p-1,-1,-1):
+                if (points[i][0]<points[p][0] and points[i][1]>=points[p][1]) or points[i][0]==points[p][0] or points[i][1]==points[p][1]:
+                    for j in range(i+1,p):
+                        if points[i][0]<=points[j][0]<=points[p][0] and points[i][1]>=points[j][1]>=points[p][1]:
+                            break
+                    else:
+                        out+=1
+
+        return out
