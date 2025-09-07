c6bcdc99c5378cfb5263d82c88a4563f2b2daf65
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-09-07 20:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (70.00%) - LeetSync

--- 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
+++ 1426-find-n-unique-integers-sum-up-to-zero/find-n-unique-integers-sum-up-to-zero.py
@@ -5,13 +5,10 @@ class Solution(object):
         :rtype: List[int]
         """
         out=[]
-        if n%2==0:
-            for i in range(1,(n/2) +1 ):
-                out.extend([i,-i])
-        else:
+        for i in range(1,(n//2) +1 ):
+            out.extend([i,-i])
+        if n%2!=0:
             out.append(0)
-            for i in range(1,(n//2) +1 ):
-                out.extend([i,-i])
         return out
 
 
