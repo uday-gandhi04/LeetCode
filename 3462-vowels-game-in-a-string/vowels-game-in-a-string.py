42a1eec66e968c04d3ca60efb0cfc106e56c7c04
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-09-13 00:27:08 +0530

    Time: 22 ms (86.36%) | Memory: 13.1 MB (11.36%) - LeetSync

--- 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ 3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-
-        count=0
-
         vowels=set(['a','e','i','o','u'])
         
         for ch in s:
             if ch in vowels:
-                count+=1
-        if count==0:
-            return False
-        return True
+                return True
+        return False
+        
