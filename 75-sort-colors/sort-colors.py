5b50c324bedba84c15f46f9dd1f6123e2e5655d2
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-09-14 00:53:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (11.36%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -10,10 +10,11 @@ class Solution(object):
         i=0
         
         while i<=p2:
-            if nums[i]==0 and i!=p1:
+            if nums[i]==0:
                 nums[p1],nums[i]=nums[i],nums[p1]
                 p1+=1
-            elif nums[i]==2 and i!=p2:
+                i+=1
+            elif nums[i]==2:
                 nums[p2],nums[i]=nums[i],nums[p2]
                 p2-=1
             else: 
