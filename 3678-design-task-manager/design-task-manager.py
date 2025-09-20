2c8f471346e3c09bca025b2fae076334c8524080
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-09-21 03:48:54 +0530

    Time: 1030 ms (20.05%) | Memory: 120.2 MB (5.01%) - LeetSync

--- 3678-design-task-manager/design-task-manager.py
+++ 3678-design-task-manager/design-task-manager.py
@@ -1,52 +1,67 @@
-import heapq
-
 class TaskManager(object):
+
     def __init__(self, tasks):
         """
         :type tasks: List[List[int]]
         """
-        self.dictTask = {}   # taskId -> [priority, userId]
-        self.heap = []       # stores (-priority, -taskId)
-        self.stale_count = 0 # number of stale heap entries
+        self.dictTask={}
+        self.heap=[]
 
-        for userId, taskId, priority in tasks:
-            self.dictTask[taskId] = [priority, userId]
-            heapq.heappush(self.heap, (-priority, -taskId))
+        for userId,taskId,priority in tasks:
+            self.dictTask[taskId]=[priority,userId]       
+            heapq.heappush(self.heap,(-priority,-taskId)) 
 
     def add(self, userId, taskId, priority):
-        self.dictTask[taskId] = [priority, userId]
-        heapq.heappush(self.heap, (-priority, -taskId))
-        self.stale_count += 1
-        if self.stale_count > len(self.heap) // 2:
-            self._rebuild_heap()
+        """
+        :type userId: int
+        :type taskId: int
+        :type priority: int
+        :rtype: None
+        """
+        self.dictTask[taskId]=[priority,userId]
+        heapq.heappush(self.heap,(-priority,-taskId))
 
     def edit(self, taskId, newPriority):
-        if taskId in self.dictTask:
-            self.dictTask[taskId][0] = newPriority
-            heapq.heappush(self.heap, (-newPriority, -taskId))
-            self.stale_count += 1
-            if self.stale_count > len(self.heap) // 2:
-                self._rebuild_heap()
+        """
+        :type taskId: int
+        :type newPriority: int
+        :rtype: None
+        """
+        self.dictTask[taskId][0]=newPriority
+        heapq.heappush(self.heap,(-newPriority,-taskId))
+        
 
     def rmv(self, taskId):
-        if taskId in self.dictTask:
-            self.dictTask[taskId][0] = -1
-            self.stale_count += 1
-            if self.stale_count > len(self.heap) // 2:
-                self._rebuild_heap()
+        """
+        :type taskId: int
+        :rtype: None
+        """
+        self.dictTask[taskId][0]=-1
+        
 
     def execTop(self):
-        while self.heap:
-            priority, neg_tid = heapq.heappop(self.heap)
-            taskId = -neg_tid
-            if taskId in self.dictTask and self.dictTask[taskId][0] == -priority:
-                user = self.dictTask[taskId][1]
-                self.dictTask[taskId][0] = -1
-                return user
-        return -1
+        """
+        :rtype: int
+        """
+        if not self.heap: 
+            return -1
+        out=heapq.heappop(self.heap)
 
-    def _rebuild_heap(self):
-        # Remove all stale entries and rebuild heap
-        self.heap = [(-priority, -tid) for tid, (priority, _) in self.dictTask.items() if priority != -1]
-        heapq.heapify(self.heap)
-        self.stale_count = 0
+        while -out[0]!=self.dictTask[-out[1]][0]:
+            if not self.heap: 
+                return -1
+            out=heapq.heappop(self.heap)
+        
+        user=self.dictTask[-out[1]][1]
+        self.dictTask[-out[1]][0]=-1
+        
+        return user
+        
+
+
+# Your TaskManager object will be instantiated and called as such:
+# obj = TaskManager(tasks)
+# obj.add(userId,taskId,priority)
+# obj.edit(taskId,newPriority)
+# obj.rmv(taskId)
+# param_4 = obj.execTop()
