d90de994a7f41f1897e2c5a78fd65a31557804e4
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-09-28 01:20:57 +0530

    Time: 521 ms (94.72%) | Memory: 12.5 MB (71.65%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -3,19 +3,38 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: int
+
+        This is a more optimal O(N^2) two-pointer approach.
         """
+        if len(nums) < 3:
+            return 0
 
         nums.sort()
-        n=len(nums)        
+        n = len(nums)
+        count = 0
 
-        out=0
+        # Iterate from the end, fixing the longest side 'c' of a potential triangle.
+        for i in range(n - 1, 1, -1):
+            c = nums[i]
+            left = 0
+            right = i - 1
 
-        for i in range(n-2):
-            for j in range(i+1,n-1):
-                x=nums[i]+nums[j]
+            # Use two pointers to find pairs (a, b) such that a + b > c.
+            while left < right:
+                a = nums[left]
+                b = nums[right]
 
-                idx=bisect.bisect_right(nums,x-1,j+1,n)
-
-                out+=idx-j-1
-        return out
-        
+                if a + b > c:
+                    # If nums[left] + nums[right] > c, then any element between
+                    # left and right will also form a valid triangle with nums[right].
+                    # For example, (nums[left+1], nums[right], c) is also valid.
+                    # So, we count all these pairs at once.
+                    count += right - left
+                    
+                    # Try a smaller second side to find more pairs.
+                    right -= 1
+                else:
+                    # The sum is too small, so we need a larger first side.
+                    left += 1
+                    
+        return count
