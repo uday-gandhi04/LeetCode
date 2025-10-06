d635ae81503b9ceb47414509767a795a4f2454a9
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-10-07 04:11:41 +0530

    Time: 43 ms (57.08%) | Memory: 12.9 MB (40.71%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -4,37 +4,36 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        n=len(grid)
+        n = len(grid)
+        # set of nodes that have already been visited so do not need to be revisited
+        visited = set()
+        # min-heap for storing the time/max height along the path, and the current coordinates
+        min_heap = [[grid[0][0], 0, 0]]
+        # directions that can be travelled in from a square
+        directions = [[0,1], [0,-1], [1,0], [-1,0]]
 
-        visited=set()
+        # add the start coordinate to the visited set
+        visited.add((0,0))
 
-        best_peak=[[float('inf')]*n for _ in range(n)]
-
-        def backtracking(i, j, peak):
-            if i < 0 or i >= n or j < 0 or j >= n or (i, j) in visited:
-                return float('inf')
-
-            peak = max(peak, grid[i][j])  # <-- Move this up first!
-
-            if peak >= best_peak[i][j]:
-                return float('inf')
-            best_peak[i][j] = peak
-
-            if i == n - 1 and j == n - 1:
-                return peak
-
-            visited.add((i, j))
-            directions = [(0,1),(1,0),(0,-1),(-1,0)]
-            out = float('inf')
-            for dx, dy in directions:
-                temp = backtracking(i + dx, j + dy, peak)
-                out = min(out, temp)
-            visited.remove((i, j))
-            return out
-
-        
-        return backtracking(0,0,grid[0][0])
-                
+        # bfs with Djikstra's algorithm
+        while min_heap:
+            # pop the current minimum time and coordinates closest to the target
+            time, r, c = heapq.heappop(min_heap)
             
+            # if the end of the grid has been reached, the time can be returned
+            if r == n-1 and c == n-1:
+                return time
+            
+            # bfs exploring the surrounding squares
+            for dr, dc in directions:
+                # neighbouring square's coordinates
+                nr, nc = r + dr, c + dc
 
-        
+                # if the square has been visited or is off the grid, skip the square
+                if (nr < 0 or nr == n or nc < 0 or nc == n or (nr,nc) in visited):
+                    continue
+                
+                # add square to the visited set
+                visited.add((nr,nc))
+                # push the path's max time and current coordinates to the min-heap
+                heapq.heappush(min_heap, [max(time, grid[nr][nc]), nr, nc])
