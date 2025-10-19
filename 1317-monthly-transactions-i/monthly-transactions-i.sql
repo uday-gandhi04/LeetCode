1b548768616485f412c63cc852890657b016f314
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-10-20 04:32:47 +0530

    Time: 625 ms (30.36%) | Memory: 0B (100.00%) - LeetSync

--- 1317-monthly-transactions-i/monthly-transactions-i.sql
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -5,4 +5,4 @@ sum(amount) as trans_total_amount,
 sum(case when state="approved" then amount else 0 end ) approved_total_amount
 
 from Transactions
-group by DATE_FORMAT(trans_date, '%Y-%m'),country
+group by month,country
