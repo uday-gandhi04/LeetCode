8a47ea8c08874e5340f9703e33610c6690ee1d6f
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-10-24 03:34:22 +0530

    Time: 637 ms (26.54%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -1,3 +1,9 @@
-select c.product_id, case when a.new_price is null then 10 else a.new_price end as price 
-from products a join (select product_id,max(change_date) as latest_date from products where change_date<='2019-08-16' group by product_id) b right join (select distinct product_id from products) c
+select c.product_id, (case when a.new_price is null then 10 else a.new_price end) as price 
+from 
+products a join 
+    (select product_id,max(change_date) as latest_date 
+    from products 
+    where change_date<='2019-08-16' 
+    group by product_id) b 
+        right join (select distinct product_id from products) c
 on a.product_id=b.product_id and a.change_date=b.latest_date and a.product_id=c.product_id
