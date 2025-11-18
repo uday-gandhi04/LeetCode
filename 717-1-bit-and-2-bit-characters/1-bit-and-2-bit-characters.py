2fa303d7c9a4a32fa974396c3d7fd166d816be1d
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-11-18 23:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (94.38%) - LeetSync

---
+++ 717-1-bit-and-2-bit-characters/1-bit-and-2-bit-characters.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isOneBitCharacter(self, bits):
+        """
+        :type bits: List[int]
+        :rtype: bool
+        """
+        i=0
+        while i<len(bits)-1:
+            if bits[i]==0:
+                i+=1
+            else:
+                i+=2
+        
+        return i==len(bits)-1
+                
+
+        
