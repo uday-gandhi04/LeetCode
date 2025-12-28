76d9667f30228e7242845a1833b45d0569be7b1e
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2025-12-28 23:47:12 +0530

    Time: 69 ms (71.21%) | Memory: 12.8 MB (91.00%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -5,7 +5,6 @@ class Solution(object):
         :rtype: int
         """
         count_y=customers.count('Y')
-        count_n=customers.count('N')
         penalty=count_y
         minn=count_y
         idx=0
