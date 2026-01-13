bc12b7fd65d4e6a096152de258f6ae2132e96da2
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2026-01-13 19:55:44 +0530

    Time: 1424 ms (76.92%) | Memory: 37.7 MB (26.92%) - LeetSync

--- 3763-separate-squares-i/separate-squares-i.py
+++ 3763-separate-squares-i/separate-squares-i.py
@@ -9,38 +9,14 @@ class Solution(object):
 
         ymin=squares[0][1]
         ymax=squares[0][1]+squares[0][2]
-        #area=[squares[0][-1]**2]*len(squares)
         total=squares[0][2]**2
         for i in range(1,len(squares)):
-            #area[i]=area[i-1]+squares[0][-1]**2
             total+=squares[i][-1]**2
             ymin=min(ymin,squares[i][1])
             ymax=max(ymax,squares[i][1]+squares[i][2])
 
-        #keys=[y[1] for y in squares]
-
         while ymax - ymin > eps:
             yo=(ymin+ymax)/2.0
-
-            """idx=bisect.bisect_left(keys,yo)
-
-
-            if idx>0 and squares[idx-1][1]+squares[idx-1][2] >yo:
-                ratio=(squares[idx-1][1]+squares[idx-1][2]-yo)/squares[idx-1][2]
-                area_below=area[idx-1]-(squares[idx-1][2]*squares[idx-1][2]*ratio)
-            elif idx>0:
-                area_below=area[idx-1]
-            else:
-                area_below=0
-
-            area_above=total-area_below
-
-            if abs(area_below-area_above)<=eps:
-                return yo
-            elif area_below<area_above:
-                ymin=yo
-            else:
-                ymax=yo"""
 
             area_below=0
             for x,y,l in squares:
@@ -57,9 +33,4 @@ class Solution(object):
             else:
                 ymax=yo
         return ymin
-            
-
-
-
-
             
