767a8887ecb5f1b7c47a322ca449ab4cdf58679f
Udayyy <121980334+uday-gandhi04@users.noreply.github.com>
2026-01-18 19:18:52 +0530

    Time: 119 ms (84.62%) | Memory: 12.6 MB (69.23%) - LeetSync

--- 1311-largest-magic-square/largest-magic-square.py
+++ 1311-largest-magic-square/largest-magic-square.py
@@ -4,40 +4,35 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        m,n=len(grid), len(grid[0])
+        row=[[0]*(n+1) for _ in range(m)]
+        col=[[0]*n for _ in range(m+1)]
 
-        def check(col,row,dia):
-            for i in range(1,len(col)):
-                if col[i-1]!=col[i] or row[i-1]!=row[i]:
+        for i in range(m):
+            for j in range(n):
+                row[i][j+1]=row[i][j]+grid[i][j]
+                col[i+1][j]=col[i][j]+grid[i][j]
+        def isMagic(r,c,k):
+            target=row[r][c+k]-row[r][c]
+
+            for i in range(r,r+k):
+                if row[i][c+k]-row[i][c]!=target:
                     return False
-            
-            return dia[0]==dia[1]==row[0]==col[0]
-
-        m,n=len(grid),len(grid[0])
-        k=min(m,n)
-
-        while k>0:
+            for j in range(c,c+k):
+                if col[r+k][j]-col[r][j]!=target:
+                    return False
+            d1=d2=0
+            for i in range(k):
+                d1+=grid[r+i][c+i]
+                d2+=grid[r+i][c+k-1-i]
+            return d1==target and d2==target 
+        for k in range(min(m, n),1,-1):
             for i in range(m-k+1):
-                for j in range(n - k + 1):
-                    row = [0] * k
-                    col = [0] * k
-                    dia = [0, 0]
-
-                    for r in range(k):
-                        for c in range(k):
-                            val = grid[i + r][j + c]
-                            row[r] += val
-                            col[c] += val
-
-                        dia[0] += grid[i + r][j + r]
-                        dia[1] += grid[i + r][j + (k - 1 - r)]
-
-                    if check(col, row, dia):
+                for j in range(n-k+1):
+                    if isMagic(i,j,k):
                         return k
-            k-=1
-        
         return 1
-                    
 
-                            
-                            
+        
 
+        
